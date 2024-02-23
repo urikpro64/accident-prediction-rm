dd54b291cd9ff4f6aab2c4af79cea2af2402197a
urikpro64 <urikpro64@gmail.com>
2024-02-23 07:07:30 +0700

    backend: setup env

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,11 @@
 from src import create_app
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = create_app()
 
-app.config['FLASK_ENV'] = 'production'
+app.config['FLASK_ENV'] = os.getenv("FLASK_ENV")
 
 if __name__ == '__main__':
     app.run(
